86b979c98baf7d20e36a2717fd41e34530870374
ingBrayanB <brabadi120413@gmail.com>
2024-10-10 21:09:39 -0500

    Agregado balanceador de carga y RDS Postgresql con salida a internet

--- Ejercicio1Terraform/config.tf
+++ Ejercicio1Terraform/config.tf
@@ -127,6 +127,13 @@ resource "aws_security_group" "my_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -145,6 +152,7 @@ resource "aws_instance" "public_ec2_1" {
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public_subnet_1.id
   key_name               = "cloud2"
+  user_data              = file("commands.sh")
   vpc_security_group_ids = [aws_security_group.my_security_group.id]
 
   tags = {
@@ -158,9 +166,129 @@ resource "aws_instance" "public_ec2_2" {
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public_subnet_2.id
   key_name               = "cloud2"
+  user_data              = file("commands2.sh")
   vpc_security_group_ids = [aws_security_group.my_security_group.id]
 
   tags = {
     Name = "PublicEC2-2"
   }
 }
+# Creamos el Load Balancer
+resource "aws_lb" "my_load_balancer" {
+  name               = "balanceadorCloud" # Cambia el nombre aquí
+  internal           = false
+  load_balancer_type = "application"
+
+  security_groups = [aws_security_group.my_security_group.id]
+  subnets = [
+    aws_subnet.public_subnet_1.id,
+    aws_subnet.public_subnet_2.id,
+  ]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "MyLoadBalancer"
+  }
+}
+
+# Creamos un target group para las instancias
+resource "aws_lb_target_group" "my_target_group" {
+  name     = "my-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc_cloud_2.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "MyTargetGroup"
+  }
+}
+
+# Registramos las instancias en el target group
+resource "aws_lb_target_group_attachment" "ec2_attachment_1" {
+  target_group_arn = aws_lb_target_group.my_target_group.arn
+  target_id        = aws_instance.public_ec2_1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "ec2_attachment_2" {
+  target_group_arn = aws_lb_target_group.my_target_group.arn
+  target_id        = aws_instance.public_ec2_2.id
+  port             = 80
+}
+
+# Creamos un listener para el Load Balancer
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.my_load_balancer.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.my_target_group.arn
+  }
+}
+
+# Creamos un grupo de subred publica para RDS
+resource "aws_db_subnet_group" "rdspostgres" {
+  name       = "rdspostgres"
+  subnet_ids = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+  tags = {
+    Name = "Rds_PostgreSQL"
+  }
+}
+
+# Creamos la instancia RDS PostgreSQL en la subred publica
+resource "aws_db_instance" "rdspostgres" {
+  identifier             = "rdspostgres"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 5
+  engine                 = "postgres"
+  engine_version         = "14.9"
+  username               = "edu"
+  password               = "Edu12345!"
+  db_subnet_group_name   = aws_db_subnet_group.rdspostgres.name
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]
+  parameter_group_name   = aws_db_parameter_group.rdspostgres.name
+  publicly_accessible    = true
+  skip_final_snapshot    = true
+}
+
+# Grupo de parametros para nuestra RDS
+resource "aws_db_parameter_group" "rdspostgres" {
+  name   = "rdspostgres"
+  family = "postgres14"
+
+  parameter {
+    name  = "log_connections"
+    value = "1"
+  }
+}
+
+# Salidas de variables
+output "rds_hostname" {
+  description = "RDS instance hostname"
+  value       = aws_db_instance.rdspostgres.address
+  sensitive   = true
+}
+
+output "rds_port" {
+  description = "RDS instance port"
+  value       = aws_db_instance.rdspostgres.port
+  sensitive   = true
+}
+
+output "rds_username" {
+  description = "RDS instance root username"
+  value       = aws_db_instance.rdspostgres.username
+  sensitive   = true
+}
+
